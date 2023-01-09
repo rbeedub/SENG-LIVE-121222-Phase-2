0f971e08189a5597313ef7c8a6ce1604bb27d684
Rebecca Wollard <rbwollard@gmail.com>
2023-01-09 12:41:04 -0700

    commit tree 42d6c001dc310e276136d40db35fcb14cf271b4c

--- 01_components_and_props/project_showcase/src/App.js
+++ 01_components_and_props/project_showcase/src/App.js
@@ -1,5 +1,12 @@
+
+import Header from "./components/Header"
+
 function App() {
   return <div>Project Showcase</div>;
+  <Header />
+  firstName = "Rebecca";
+  lastName = "Miller";
+  age = {32}
 }
 
 export default App;
---
+++ 01_components_and_props/project_showcase/src/components/Header.js
@@ -0,0 +1,7 @@
+const Header = (props) => {
+    console.log(props)
+    return (
+        <h2>Header</h2>
+    )
+}
+export default Header;
